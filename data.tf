625de8e164430e9fbeca9333bd88e4f51cd7b56c
Joygit <joy.chakraborty.3520@gmail.com>
2026-03-27 13:54:30 +0530

    Comment out automation account data configuration
    Comment out azurerm_automation_account data block

--- data.tf
+++ data.tf
@@ -2,7 +2,7 @@ data "azurerm_client_config" "current" {
 
 }
 
-data "azurerm_automation_account" "aa" {
-  name                = var.automation_account_name
-  resource_group_name = var.resource_group_name
-}
+#data "azurerm_automation_account" "aa" {
+#  name                = var.automation_account_name
+#  resource_group_name = var.resource_group_name
+#}
