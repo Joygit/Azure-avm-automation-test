dc5f895fcde238e0fa115ea78512303c5569ffdd
Joygit <joy.chakraborty.3520@gmail.com>
2026-03-27 13:13:06 +0530

    Add azurerm_automation_account data source

--- data.tf
+++ data.tf
@@ -1,3 +1,8 @@
 data "azurerm_client_config" "current" {
 
 }
+
+data "azurerm_automation_account" "aa" {
+  name                = var.automation_account_name
+  resource_group_name = var.resource_group_name
+}
