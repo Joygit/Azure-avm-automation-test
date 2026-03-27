2c0943752811f5a8feb572e9b5d5c746a2acf956
Joygit <joy.chakraborty.3520@gmail.com>
2026-03-27 13:18:27 +0530

    Comment out DSC outputs in output.tf
    Comment out outputs for DSC endpoint and primary access key.

--- output.tf
+++ output.tf
@@ -8,13 +8,13 @@ output "automation_account_name" {
   value       = var.automation_account_name
 }
 
-output "automation_account_dsc_server_endpoint" {
-  description = "DSC endpoint"
-  value       = data.azurerm_automation_account.aa.dsc_server_endpoint
-}
+#output "automation_account_dsc_server_endpoint" {
+#  description = "DSC endpoint"
+#  value       = data.azurerm_automation_account.aa.dsc_server_endpoint
+#}
 
-output "automation_account_dsc_primary_access_key" {
-  description = "DSC primary key"
-  value       = data.azurerm_automation_account.aa.dsc_primary_access_key
-  sensitive   = true
-}
+#output "automation_account_dsc_primary_access_key" {
+#  description = "DSC primary key"
+#  value       = data.azurerm_automation_account.aa.dsc_primary_access_key
+#  sensitive   = true
+#}
