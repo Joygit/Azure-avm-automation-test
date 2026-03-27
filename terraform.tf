0f8e74f6d0feb706dca142376176c643447a817f
Joygit <joy.chakraborty.3520@gmail.com>
2026-03-27 12:26:38 +0530

    Fix typo in azurerm provider source

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0"
   required_providers {
     azurerm = {
-      source  = "hasicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~>2.2"
     }
     azapi = {
